0b601848a64ac884a64940a7e28365860c45cfb5
Nathalie <natg.inf@gmail.com>
2017-09-03 15:58:16 +0200

    Add timer

--- js/memory.js
+++ js/memory.js
@@ -6,21 +6,39 @@
     const cards = ['anchor', 'balance-scale', 'bath', 'battery-2', 'bed', 'beer', 'bell', 'bicycle', 'binoculars', 'birthday-cake', 'bomb', 'book', 'briefcase', 'bug', 'bus', 'cab', 'camera', 'camera-retro', 'car', 'cloud', 'codepen', 'coffee', 'cogs', 'compass', 'credit-card', 'cubes', 'cutlery', 'diamond', 'dribble', 'eye', 'film', 'fire', 'fire-extinguisher', 'flask', 'gamepad', 'gears', 'gift', 'github', 'glass', 'hand-spock-o', 'heart', 'key', 'leaf', 'legal', 'life-buoy', 'lightbulb-o', 'linode', 'magic', 'map-signs', 'medkit', 'microchip', 'microphone', 'modx', 'moon', 'mortar-board', 'motorcycle', 'music', 'pagelines', 'paint-brush', 'paw', 'pied-piper-alt', 'plane', 'plug', 'puzzle-piece', 'rocket', 'scissors', 'ser-secret', 'ship', 'shower', 'snowflake-o', 'subway', 'suitcase', 'superpowers', 'thermometer-3', 'ticket', 'trash', 'train', 'tree', 'trophy', 'truck', 'tv', 'umbrella', 'university', 'venus-double', 'video-camera', 'wrench'];
 
 
-    let ScoreBoard = function() {
+    let ScoreBoard = function () {
         this.rating = 0;
         this.timer = 0;
+        this.resetTimer = function () {
+            clearInterval(this.updateTimer);
+            clearInterval(this.timer);
+            this.timer = 0;
+        };
+        this.updateTimer = function () {
+            let secs = 0;
+            let mins = 0;
+            this.timer = setInterval(function () {
+                secs++;
+                if (secs == 60) {
+                    secs = 0;
+                    mins++;
+                }
+                document.getElementById('timer').innerHTML = mins + ':' + ("0" + secs).slice(-2);;
+
+            }, 1000);
+        };
     };
 
-     let CardSet = function () {
-         this.length = 8;
-         this.cards = [];
-         this.moves = 0;
-         this.found = 0;
+    let CardSet = function () {
+        this.length = 8;
+        this.cards = [];
+        this.moves = 0;
+        this.found = 0;
     };
 
     //instantiate new cardset and scoreboard
-        let cardSet;
-        let scoreBoard;
+    let cardSet;
+    let scoreBoard;
 
     listenButton();
 
@@ -31,15 +49,21 @@
         })
     }
 
-
     function clearBoard() {
+
         document.getElementById('deck').innerHTML = '';
+        if (scoreBoard instanceof ScoreBoard) {
+            scoreBoard.resetTimer();
+        }
+        // set new cards and scoreboard
+        cardSet = new CardSet();
+        scoreBoard = new ScoreBoard();
         prepareGame();
     }
 
     function prepareGame() {
-        cardSet = new CardSet();
-        scoreBoard = new ScoreBoard();
+
+        scoreBoard.updateTimer();
         //retrieve level selection (==nb of pairs to play with)
         var select = document.getElementById('nbPairs');
         //put the user choice in the card set
@@ -54,7 +78,7 @@
             randomNb++;
         }
         shuffle(cardSet.cards);
-        console.log(cardSet.cards);
+//        console.log(cardSet.cards);
         displayCards();
     }
 
@@ -77,7 +101,7 @@
 
     function displayCards() {
         const deck = document.getElementById('deck');
-        for (let i = 0; i < cardSet.cards.length; i++){
+        for (let i = 0; i < cardSet.cards.length; i++) {
             let currentCard = document.createElement('div');
             currentCard.className = 'card';
             document.getElementById('deck').appendChild(currentCard);
