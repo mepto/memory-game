e8bfcccbf10554eb6f21edb4dc145e9cd2eec24d
Nathalie <natg.inf@gmail.com>
2017-09-03 13:44:17 +0200

    add cards shuffle and display

--- js/memory.js
+++ js/memory.js
@@ -1,67 +1,107 @@
 'use strict';
-listenButton();
 
-function listenButton() {
-    document.getElementById('startGame').addEventListener("click", function(e){
-        e.preventDefault();
+(function () {
+
+    //but why can shuffle modify it when it's defined as const?
+    const cards = ['anchor', 'balance-scale', 'bath', 'battery-2', 'bed', 'beer', 'bell', 'bicycle', 'binoculars', 'birthday-cake', 'bomb', 'book', 'briefcase', 'bug', 'bus', 'cab', 'camera', 'camera-retro', 'car', 'cloud', 'codepen', 'coffee', 'cogs', 'compass', 'credit-card', 'cubes', 'cutlery', 'diamond', 'dribble', 'eye', 'film', 'fire', 'fire-extinguisher', 'flask', 'gamepad', 'gears', 'gift', 'github', 'glass', 'hand-spock-o', 'heart', 'key', 'leaf', 'legal', 'life-buoy', 'lightbulb-o', 'linode', 'magic', 'map-signs', 'medkit', 'microchip', 'microphone', 'modx', 'moon', 'mortar-board', 'motorcycle', 'music', 'pagelines', 'paint-brush', 'paw', 'pied-piper-alt', 'plane', 'plug', 'puzzle-piece', 'rocket', 'scissors', 'ser-secret', 'ship', 'shower', 'snowflake-o', 'subway', 'suitcase', 'superpowers', 'thermometer-3', 'ticket', 'trash', 'train', 'tree', 'trophy', 'truck', 'tv', 'umbrella', 'university', 'venus-double', 'video-camera', 'wrench'];
+
+
+    let ScoreBoard = function() {
+        this.rating = 0;
+        this.timer = 0;
+    };
+
+     let CardSet = function () {
+         this.length = 8;
+         this.cards = [];
+         this.moves = 0;
+         this.found = 0;
+    };
+
+    //instantiate new cardset and scoreboard
+        let cardSet;
+        let scoreBoard;
+
+    listenButton();
+
+    function listenButton() {
+        document.getElementById('startGame').addEventListener("click", function (e) {
+            e.preventDefault();
+            clearBoard();
+        })
+    }
+
+
+    function clearBoard() {
+        document.getElementById('deck').innerHTML = '';
+        prepareGame();
+    }
+
+    function prepareGame() {
+        cardSet = new CardSet();
+        scoreBoard = new ScoreBoard();
+        //retrieve level selection (==nb of pairs to play with)
+        var select = document.getElementById('nbPairs');
+        //put the user choice in the card set
+        cardSet.length = select.options[select.selectedIndex].value;
+        //get a random start number and shuffle the cards array
+        let randomNb = Math.floor(Math.random() * ((cards.length - cardSet.length) + 1));
+        shuffle(cards);
+        //start populating the cardset array
+        for (let i = 0; i < cardSet.length; i++) {
+            cardSet.cards.push(cards[randomNb]);
+            cardSet.cards.push(cards[randomNb]);
+            randomNb++;
+        }
+        shuffle(cardSet.cards);
+        console.log(cardSet.cards);
         displayCards();
-    })
-}
+    }
 
-var cards = ['anchor', 'balance-scale', 'bath', 'battery-2', 'bed', 'beer', 'bell', 'bicycle', 'binoculars', 'birthday-cake', 'bomb', 'book', 'briefcase', 'bug', 'bus', 'cab', 'camera', 'camera-retro', 'car', 'cloud', 'codepen', 'coffee', 'cogs', 'compass', 'credit-card', 'cubes', 'cutlery', 'diamond', 'dribble', 'eye', 'film', 'fire', 'fire-extinguisher', 'flask', 'gamepad', 'gears', 'gift', 'github', 'glass', 'hand-spock-o', 'heart', 'key', 'leaf', 'legal', 'life-buoy', 'lightbulb-o', 'linode', 'magic', 'map-signs', 'medkit', 'microchip', 'microphone', 'modx', 'moon', 'mortar-board', 'motorcycle', 'music', 'pagelines', 'paint-brush', 'paw', 'pied-piper-alt', 'plane', 'plug', 'puzzle-piece', 'rocket', 'scissors', 'ser-secret', 'ship', 'shower', 'snowflake-o', 'subway', 'suitcase', 'superpowers', 'thermometer-3', 'ticket', 'trash', 'train', 'tree', 'trophy', 'truck', 'tv', 'umbrella', 'university', 'venus-double', 'video-camera', 'wrench'];
+    //https://bost.ocks.org/mike/shuffle/compare.html
+    function shuffle(allCards) {
+        var currentCard = allCards.length,
+            swap, remainingCard;
 
-function displayCards() {
-    clearBoard();
-    var select = document.getElementById('nbPairs');
-    var nbPairs = select.options[select.selectedIndex].value;
-   console.log(nbPairs);
-   // console.log(shuffle(cards));
-}
+        // While there remain elements to shuffle…
+        while (currentCard) {
+            // Pick a remaining card
+            remainingCard = Math.floor(Math.random() * currentCard--);
+            // And swap it with the current card
+            swap = allCards[currentCard];
+            allCards[currentCard] = allCards[remainingCard];
+            allCards[remainingCard] = swap;
+        }
+        return allCards;
+    }
 
-function clearBOard() {
+    function displayCards() {
+        const deck = document.getElementById('deck');
+        for (let i = 0; i < cardSet.cards.length; i++){
+            let currentCard = document.createElement('div');
+            currentCard.className = 'card';
+            document.getElementById('deck').appendChild(currentCard);
+            document.getElementsByClassName('card')[i].innerHTML = '\
+                    <div class="card-back"></div>\
+                    <div class="card-front"><i class="fa fa-' + cardSet.cards[i] + '"></i></div>';
+        }
 
-}
-
-let CardSet = function() {
-    this.length = 8;
-};
-
-//https://bost.ocks.org/mike/shuffle/compare.html
-function shuffle(allCards) {
-  var currentElement = allCards.length, swap, remainingElement;
-
-  // While there remain elements to shuffle…
-  while (currentElement) {
-
-    // Pick a remaining element…
-    remainingElement = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    swap = allCards[currentElement];
-    allCards[currentElement] = allCards[remainingElement];
-    allCards[remainingElement] = swap;
-  }
-
-  return allCards;
-}
+    }
 
 
-function flipCard(elem) {
+    function flipCard(elem) {
 
-}
+    }
 
-function checkMatch() {
+    function checkMatch() {
 
-}
+    }
 
-////https://bost.ocks.org/mike/shuffle/compare.html
-//function shuffle(arr) {
-//    const m = arr.length,
-//        t, i;
-//    while (m) {
-//        i = Math.floor(Math.random() * m--);
-//        t = arr[m];
-//        arr[m] = arr[i];
-//        arr[i] = t;
-//    }
-//}
+    function updateRating() {
+
+    }
+
+    //add stuff to local storage?
+
+
+})();
