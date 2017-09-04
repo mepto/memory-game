c20a2e9dce49ac6f2debcb285223cc9600d617ff
Nathalie <natg.inf@gmail.com>
2017-09-04 20:19:01 +0200

    Add rating

--- js/memory.js
+++ js/memory.js
@@ -3,11 +3,26 @@
 (function () {
 
     //but why can shuffle modify it when it's defined as const?
-    const cards = ['anchor', 'balance-scale', 'bath', 'battery-2', 'bed', 'beer', 'bell', 'bicycle', 'binoculars', 'birthday-cake', 'bomb', 'book', 'briefcase', 'bug', 'bus', 'cab', 'camera', 'camera-retro', 'car', 'cloud', 'codepen', 'coffee', 'cogs', 'compass', 'credit-card', 'cubes', 'cutlery', 'diamond', 'dribbble', 'eye', 'film', 'fire', 'fire-extinguisher', 'flask', 'gamepad',  'gift', 'github', 'glass', 'hand-spock-o', 'heart', 'key', 'leaf', 'legal', 'life-buoy', 'lightbulb-o', 'linode', 'magic', 'map-signs', 'medkit', 'microchip', 'microphone', 'modx', 'moon-o', 'mortar-board', 'motorcycle', 'music', 'pagelines', 'paint-brush', 'paw', 'pied-piper-alt', 'plane', 'plug', 'puzzle-piece', 'rocket', 'scissors', 'user-secret', 'ship', 'shower', 'snowflake-o', 'subway', 'suitcase', 'superpowers', 'thermometer-three-quarters', 'ticket', 'trash', 'train', 'tree', 'trophy', 'truck', 'tv', 'umbrella', 'university', 'venus-double', 'video-camera', 'wrench'];
+    const cards = ['anchor', 'balance-scale', 'bath', 'battery-2', 'bed', 'beer', 'bell', 'bicycle', 'binoculars', 'birthday-cake', 'bomb', 'book', 'briefcase', 'bug', 'bus', 'cab', 'camera', 'camera-retro', 'car', 'cloud', 'codepen', 'coffee', 'cogs', 'compass', 'credit-card', 'cubes', 'cutlery', 'diamond', 'dribbble', 'eye', 'film', 'fire', 'fire-extinguisher', 'flask', 'gamepad', 'gift', 'github', 'glass', 'hand-spock-o', 'heart', 'key', 'leaf', 'legal', 'life-buoy', 'lightbulb-o', 'linode', 'magic', 'map-signs', 'medkit', 'microchip', 'microphone', 'modx', 'moon-o', 'mortar-board', 'motorcycle', 'music', 'pagelines', 'paint-brush', 'paw', 'pied-piper-alt', 'plane', 'plug', 'puzzle-piece', 'rocket', 'scissors', 'user-secret', 'ship', 'shower', 'snowflake-o', 'subway', 'suitcase', 'superpowers', 'thermometer-three-quarters', 'ticket', 'trash', 'train', 'tree', 'trophy', 'truck', 'tv', 'umbrella', 'university', 'venus-double', 'video-camera', 'wrench'];
 
 
     let ScoreBoard = function () {
+        //rating management
         this.rating = 0;
+        this.resetRating = function () {
+            this.rating = 0;
+        };
+        this.updateRating = function () {
+            document.getElementById('rating').innerHTML = '';
+            for (let i = 1; i <= 3; i++) {
+                if (this.rating >= i) {
+                    document.getElementById('rating').innerHTML += '<i class="fa fa-star">';
+                } else {
+                    document.getElementById('rating').innerHTML += '<i class="fa fa-star-o">';
+                }
+            }
+        };
+        //timer management
         this.timer = 0;
         this.resetTimer = function () {
             clearInterval(this.updateTimer);
@@ -23,9 +38,29 @@
                     secs = 0;
                     mins++;
                 }
-                document.getElementById('timer').innerHTML = mins + ':' + ("0" + secs).slice(-2);;
-
+                document.getElementById('timer').innerHTML = mins + ':' + ("0" + secs).slice(-2);
             }, 1000);
+        };
+        //moves management
+        this.moves = 0;
+        this.updateMoves = function () {
+            this.moves += 1;
+            document.getElementById('moves').innerHTML = this.moves;
+            if (this.moves < cardSet.length * 2) {
+                this.rating = 3;
+                console.log(this.rating);
+            } else if (this.moves < cardSet.length * 3) {
+                this.rating = 2;
+                console.log(this.rating);
+            } else {
+                this.rating = 1;
+                console.log(this.rating);
+            }
+            this.updateRating();
+        };
+        this.resetMoves = function () {
+            this.moves = 0;
+            document.getElementById('moves').innerHTML = this.moves;
         };
     };
 
@@ -33,19 +68,10 @@
         this.length = 8;
         this.cards = [];
         this.cardsToMatch = [];
-        this.moves = 0;
-        this.updateMoves = function() {
-            this.moves +=1;
-            document.getElementById('moves').innerHTML = this.moves;
-        };
-        this.found = 0;
         this.resetCardSet = function () {
             this.cards = [];
             this.cardsToMatch = [];
-            this.moves = 0;
-            document.getElementById('moves').innerHTML = this.moves;
-            this.found = 0;
-        }
+        };
     };
 
     //instantiate new cardset and scoreboard
@@ -74,6 +100,8 @@
         document.getElementById('deck').innerHTML = '';
         if (scoreBoard instanceof ScoreBoard) {
             scoreBoard.resetTimer();
+            scoreBoard.resetMoves();
+            scoreBoard.resetRating();
             cardSet.resetCardSet();
         }
         // set new cards and scoreboard
@@ -107,7 +135,6 @@
     function shuffle(allCards) {
         let currentCard = allCards.length,
             swap, remainingCard;
-
         // While there remain elements to shuffle…
         while (currentCard) {
             // Pick a remaining card
@@ -133,7 +160,6 @@
         listenCards();
     }
 
-
     function flipCard(elem) {
 
         if (!(elem.classList.contains("turned"))) {
@@ -142,7 +168,10 @@
                 const currentCard = elem.childNodes[3].childNodes[0];
                 cardSet.cardsToMatch.push(currentCard);
                 if (cardSet.cardsToMatch.length == 2) {
-                    checkMatch();
+                    setTimeout(function () {
+                        checkMatch();
+                    }, 600);
+
                 }
             }
         }
@@ -153,34 +182,44 @@
         for (let i = 0; i < cardSet.cardsToMatch.length; i++) {
             turnedCards.push(cardSet.cardsToMatch[i].parentNode.parentNode);
         }
-
-
         if (cardSet.cardsToMatch[0].outerHTML === cardSet.cardsToMatch[1].outerHTML) {
             for (let i = 0; i < turnedCards.length; i++) {
                 turnedCards[i].classList.add('matched-card');
             }
-            gameOver();
+            setTimeout(function () {
+                checkEndGame();
+            }, 600);
+
         } else {
-            console.log('different');
+            //            console.log('different');
             for (let i = 0; i < turnedCards.length; i++) {
                 turnedCards[i].classList.add('wrong-card');
             }
             setTimeout(function () {
                 for (let i = 0; i < turnedCards.length; i++) {
-                    turnedCards[i].classList.remove('wrong-card', 'turned');
+                    turnedCards[i].classList.remove('wrong-card');
                 }
-            }, 1500);
+                for (let i = 0; i < turnedCards.length; i++) {
+                    turnedCards[i].classList.add('unturned');
+                }
+                setTimeout(function () {
+                    for (let i = 0; i < turnedCards.length; i++) {
+                        turnedCards[i].classList.remove('unturned', 'turned');
+                    }
+                }, 1000);
+            }, 1000);
+
         }
         cardSet.cardsToMatch = [];
-        cardSet.updateMoves();
+        scoreBoard.updateMoves();
     }
 
-    function updateRating() {
-
-    }
-
-    function gameOver() {
-
+    function checkEndGame() {
+        const deck = document.getElementsByClassName('matched-card');
+        if (deck.length === (cardSet.length * 2)) {
+            clearInterval(scoreBoard.timer);
+            alert('terminé');
+        }
     }
 
     //add stuff to local storage?
